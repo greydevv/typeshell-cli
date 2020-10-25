8337d155b361c3d0a219f9bc1719fc953efca263
Greyson Murray <greyson.murray@gmail.com>
2020-10-25 09:19:20 -0400

    increase version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'typeshell-cli',
-    version = '0.1.0',
+    version = '0.1.1',
     author='Greyson Murray',
     author_email='greysonmurray.dev@gmail.com',
     description='Typing test CLI tool.',
