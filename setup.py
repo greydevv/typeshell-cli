7a61c47a9c3066d24bec4df29bbb12a5088c0496
Greyson Murray <greyson.murray@gmail.com>
2020-10-25 09:34:02 -0400

    reset version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name = 'typeshell',
-    version = '0.1.3',
+    version = '0.1.0',
     author='Greyson Murray',
     author_email='greysonmurray.dev@gmail.com',
     description='Typing test CLI tool.',
